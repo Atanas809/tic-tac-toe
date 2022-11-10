0f4ab360b4f942b3c1438b9f00446bb13fbc04d2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-10 10:31:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,3 +60,18 @@ def print_table(board, size):
                     current_row += f"|  {sign}  |"
             index += 1
         print(current_row)
+
+
+def apply_move(position, board, sign):
+
+    possible_moves = {
+        1: (0, 0),
+        2: (0, 1),
+        3: (0, 2),
+        4: (1, 0),
+        5: (1, 1),
+        6: (1, 2),
+        7: (2, 0),
+        8: (2, 1),
+        9: (2, 2),
+    }
