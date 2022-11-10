fb9c57456c15b4ff2ba211ef26510a9cb55f0766
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-10 10:30:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ def is_draw(board):
         return True
     else:
         return False
+
+
+
+def is_win(board, sign):
+
+    possible_wins = {
+        "horizontal": [
+            [board[0][0], board[0][1], board[0][2]],
+            [board[1][0], board[1][1], board[1][2]],
+            [board[2][0], board[2][1], board[2][2]],
+        ],
+        "vertical": [
+            [board[0][0], board[1][0], board[2][0]],
+            [board[0][1], board[1][1], board[2][1]],
+            [board[0][2], board[1][2], board[2][2]],
+        ],
+        "diagonal": [
+            [board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]],
+        ]
+    }
